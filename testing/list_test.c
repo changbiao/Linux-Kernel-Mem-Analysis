5464428e5fe1bda2b2dc841080acc55f32996231
Anthony <amh339@kerrigan.(none)>
2012-10-30 19:49:28 -0400

    Modified Test Code

--- testing/list_test.c
+++ testing/list_test.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define SIZE 4096
+
+int list[SIZE];
+
 int main()
 {
-	int size = 4096;
-	int faults = 5;
-	int j = 0;
-	int i = 0;
-	int * heap;
-
-	
-	heap = (int *) malloc(faults * size * sizeof(int));
-	for(i = 0; i < size; i=i+4096)
+	int i;
+	for(i = 0; i < SIZE; i++)
 	{
-		heap[i] = j;	
-		j++;
+		list[i] = i;
 	}
 }
