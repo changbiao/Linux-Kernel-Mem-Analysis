40d199c6646eb5e1aa5abbec1731282160724b40
Anthony <amh339@kerrigan.(none)>
2012-11-04 19:34:09 -0500

    Array Version

--- kmodule/module.c
+++ kmodule/module.c
@@ -5,15 +5,20 @@
 #include <linux/sched.h>
 #include <linux/linkage.h>
 
-#define BUF_LEN 128
+#define STORAGE 2
+#define BUF_LEN 1024
+#define BUF_LEN_I 256
 
 /* Name of the proc file */
 static char MODNAME[] = "mem_hurst";
 
-unsigned int record_;
-int pid_;
-unsigned long address_;
-unsigned long error_code_;
+struct my_mem_info
+{
+	unsigned int record;
+	int pid;
+	unsigned long address;
+	unsigned long error_code;
+};
 
 
 
@@ -23,13 +28,17 @@ extern void (*STUB_do_page_fault_injection)(unsigned long, struct task_struct *,
 void do_page_fault_injection(unsigned long address, struct task_struct *tsk, unsigned long error_code);
 
 unsigned int i = 0;
+struct my_mem_info data_store[STORAGE];
 
 void do_page_fault_injection(unsigned long address, struct task_struct *tsk, unsigned long error_code)
 {
-	record_ = i;
-	pid_ = tsk->pid;
-	address_ = address;
-	error_code_ = error_code;
+	int pos = i % STORAGE;
+	struct my_mem_info mi;
+	mi.record = i;
+	mi.pid = tsk->pid;
+	mi.address = address;
+	mi.error_code = error_code;
+	data_store[pos] = mi;
 	i++;
 }
 
@@ -37,24 +46,44 @@ void do_page_fault_injection(unsigned long address, struct task_struct *tsk, uns
 
 
 
-static unsigned int last = 0;
+
 static int get_info(char *sys_buffer, char **my_buffer, off_t file_pos, int my_buffer_length)
 {
+    STUB_do_page_fault_injection = NULL;
+    static char buffer[BUF_LEN];
+    int len = 0;
 
-	static char buffer[BUF_LEN];
-	static int len = 0;
-	if(last == 0 || last != record_)
-	{
-		int bit0 = error_code_ & 0x1;
-		int bit1 = error_code_ & 0x2 >> 1;
-		int bit2 = error_code_ & 0x4 >> 2;
-		int bit3 = error_code_ & 0x8 >> 3;
-		int bit4 = error_code_ & 0x10 >> 4;
-		len = sprintf(buffer, "record: %u pid: %d address: %lu bit0: %d bit1: %d bit2: %d bit3: %d bit4: %d\n", record_, pid_, address_, bit0, bit1, bit2, bit3, bit4);
-		last = record_;
-	}
-	*my_buffer = buffer;
-	return len;
+    unsigned int n = i;
+    if(i > STORAGE)
+    {
+       n = STORAGE;
+    }
+
+    int j;
+    for(j = 0; j < n; j++)
+    {
+	char buffer_i[BUF_LEN_I-1];
+	unsigned int record = data_store[j].record;
+	int pid_ = data_store[j].pid;
+	unsigned long address_ = data_store[j].address;
+	unsigned long error_code_ = data_store[j].error_code;
+	int bit0 = error_code_ & 0x1;
+	int bit1 = error_code_ & 0x2 >> 1;
+	int bit2 = error_code_ & 0x4 >> 2;
+	int bit3 = error_code_ & 0x8 >> 3;
+	int bit4 = error_code_ & 0x10 >> 4;
+        len = len +  snprintf(buffer_i, BUF_LEN_I-1, "record: %u, pid: %d, address: %lu, bit0: %d, bit1: %d, bit2: %d, bit3: %d, bit4: %d\n", record, pid_, address_, bit0, bit1, bit2, bit3, bit4);
+	strncat(buffer, buffer_i, BUF_LEN_I-1);
+    }  
+
+    *my_buffer = buffer;
+
+    if(file_pos > 0)
+    {
+        return 0;
+    }
+    STUB_do_page_fault_injection=&(do_page_fault_injection);
+    return len;
 }
 
 int __init init_module(void)
